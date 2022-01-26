cf361790d99412160c05a80446e1463bced92cc3
Maksim Khramov <maxim.khramov@mail.ru>
2022-01-26 19:49:02 +0300

    add HTMLProps interface

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,12 +2,17 @@ import { ButtonProps } from './Button.props';
 import styles from './Button.module.css';
 import cn from 'classnames';
 
-export const Button = ({ appearance, children }: ButtonProps): JSX.Element => {
-    return (
-        <button className={cn({
-            [styles.button]: true,
-            [styles.primary]: appearance === 'primary',
-            [styles.ghost]: appearance === 'ghost',
-        })}>{children}</button>
-    );
+export const Button = ({ appearance, children, className, ...props }: ButtonProps): JSX.Element => {
+  return (
+    <button
+      className={cn(className, {
+        [styles.button]: true,
+        [styles.primary]: appearance === 'primary',
+        [styles.ghost]: appearance === 'ghost',
+      })}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 };
