fc7c65a33bd7bcc8004fc8cfa28f974cf39bc580
Maksim Khramov <maxim.khramov@mail.ru>
2022-02-23 17:28:33 +0300

    add with layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -3,8 +3,9 @@ import styles from './Layout.module.css';
 import { Header } from './Header/Header';
 import { Sidebar } from './Sidebar/Sidebar';
 import { Footer } from './Footer/Footer';
+import { FunctionComponent } from 'react';
 
-export const Layout = ({ children }: LayoutProps): JSX.Element => {
+const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
     <>
       <Header />
@@ -15,4 +16,14 @@ export const Layout = ({ children }: LayoutProps): JSX.Element => {
       <Footer />
     </>
   );
+};
+
+export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+  return function withLayoutComponent(props: T): JSX.Element {
+    return (
+      <Layout>
+        <Component {...props} />
+      </Layout>
+    );
+  };
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import { Htag, Button, P, Tag, Rating } from '../components';
 import { useState } from 'react';
-import { Layout } from '../layout/Layout';
+import { withLayout } from '../layout/Layout';
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
   const [rating, setRating] = useState<number>(0);
 
   return (
-    <Layout>
+    <>
       <Htag tag='h1'>Hello world</Htag>
       <Button appearance='primary'>Primary</Button>
       <Button appearance='ghost' arrow='right'>Ghost</Button>
@@ -28,6 +28,8 @@ export default function Home(): JSX.Element {
         <Rating rating={rating} setRating={setRating} isEditable />
         <Rating rating={3} />
       </div>
-    </Layout>
+    </>
   );
 }
+
+export default withLayout(Home);
