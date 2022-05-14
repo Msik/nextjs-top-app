52b2da4875ae73665df3e8cacda335228de0deba
Maksim Khramov <maxim.khramov@mail.ru>
2022-05-14 16:59:49 +0300

    add price format

--- components/Tag/Tag.props.ts
+++ components/Tag/Tag.props.ts
@@ -3,6 +3,6 @@ import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
 export interface TagProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   size?: 's' | 'm';
   children: ReactNode;
-  color: 'ghost' | 'red' | 'gray' | 'green' | 'primary';
+  color?: 'ghost' | 'red' | 'gray' | 'green' | 'primary';
   href?: string;
 }
