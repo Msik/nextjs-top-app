197d6a9760c3bfb6d4df77e68faa5fda6b996161
Maksim Khramov <maxim.khramov@mail.ru>
2022-08-23 20:18:04 +0300

    add reset action for sort reducer

--- page-component/TopPageComponent/TopPageComponent.tsx
+++ page-component/TopPageComponent/TopPageComponent.tsx
@@ -3,7 +3,7 @@ import { TopPageComponentProps } from './TopPageComponent.props';
 import styles from './TopPageComponent.module.css';
 import { TopLevelCategory } from '../../interfaces/page.interface';
 import { SortEnum } from '../../components/Sort/Sort.props';
-import { useReducer } from 'react';
+import { useReducer, useEffect } from 'react';
 import { sortReducer } from './sort.reducer';
 
 export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
@@ -12,6 +12,10 @@ export const TopPageComponent = ({ page, products, firstCategory }: TopPageCompo
   const setSort = (sort: SortEnum) => {
     dispatchSort({ type: sort });
   };
+
+  useEffect(() => {
+    dispatchSort({ type: 'reset', initialState: products });
+  }, [products]);
 
   return (
     <div className={styles.wrapper}>
