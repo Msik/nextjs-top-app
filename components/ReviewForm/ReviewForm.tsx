510611bf1e945f6dd4312eb69e6334eac3e2b08a
Maksim Khramov <maxim.khramov@mail.ru>
2022-10-17 18:14:46 +0300

    review form alerts availability

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -82,18 +82,30 @@ export const ReviewForm = ({ productId, isOpened, className, ...props }: ReviewF
           <span className={styles.info}>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
         </div>
       </div>
-      {isSuccess && <div className={cn(styles.panel, styles.success)}>
+      {isSuccess && <div className={cn(styles.panel, styles.success)} role='alert'>
         <div className={styles.successTitle}>Ваш отзыв отправлен</div>
         <div>
           Спасибо, ваш отзыв будет опубликован после проверки.
         </div>
-        <CloseIcon className={styles.close} onClick={() => setIsSuccess(false)} />
+        <button
+          className={styles.close}
+          onClick={() => setIsSuccess(false)}
+          aria-label='Закрыть оповещение'
+        >
+          <CloseIcon />
+        </button>
       </div>}
-      {error && <div className={cn(styles.panel, styles.error)}>
+      {error && <div className={cn(styles.panel, styles.error)} role='alert'>
         <div>
           Что то пошло не так, попробуйте обновить страницу
         </div>
-        <CloseIcon className={styles.close} onClick={() => setError('')} />
+        <button
+          className={styles.close}
+          onClick={() => setError('')}
+          aria-label='Закрыть оповещение'
+        >
+          <CloseIcon />
+        </button>
       </div>}
     </form>
   );
