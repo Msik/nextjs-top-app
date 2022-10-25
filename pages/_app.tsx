54434d7db2985df458ffb703bbff51167eb74050
Maksim Khramov <maxim.khramov@mail.ru>
2022-10-25 19:22:14 +0300

    add meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from 'next/dist/shared/lib/router/router';
 import Head from 'next/head';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
   return (
     <>
       <Head>
@@ -12,6 +12,8 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link href='https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap' rel='stylesheet' />
+        <meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
+        <meta property='og:locale' content='ru_RU' />
       </Head>
       <Component {...pageProps} />
     </>
