274cb6c5c7175a471ca1ef414a476fe76514493a
Maksim Khramov <maxim.khramov@mail.ru>
2022-10-25 22:27:56 +0300

    add yandex metrika

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
 import Head from 'next/head';
+import ym, { YMInitializer } from 'react-yandex-metrika';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
+  router.events.on('routeChangeComplete', (url: string) => {
+    if (typeof window !== 'undefined') {
+      ym('hit', url);
+    }
+  });
+
   return (
     <>
       <Head>
@@ -11,11 +18,17 @@ function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
         <link rel='icon' href='/favicon.ico' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link rel='preconnect' href='https://mc.yandex.ru' />
         <link href='https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap' rel='stylesheet' />
         <meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
         <meta property='og:locale' content='ru_RU' />
       </Head>
       <Component {...pageProps} />
+      <YMInitializer
+        accounts={[]}
+        options={{ webvisor: true, defer: true }}
+        version='2'
+      />
     </>
   );
 }
