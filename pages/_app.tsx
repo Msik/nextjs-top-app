82fbdeb294c1413676c566b91ae10979940baa28
Maksim Khramov <maxim.khramov@mail.ru>
2022-11-04 15:06:53 +0300

    add docker compose

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
 import Head from 'next/head';
-import Router from 'next/router';
-import ym, { YMInitializer } from 'react-yandex-metrika';
+// import Router from 'next/router';
+// import ym, { YMInitializer } from 'react-yandex-metrika';
 import '../styles/globals.css';
 
-Router.events.on('routeChangeComplete', (url: string) => {
-  if (typeof window !== 'undefined') {
-    ym('hit', url);
-  }
-});
+// Router.events.on('routeChangeComplete', (url: string) => {
+//   if (typeof window !== 'undefined') {
+//     ym('hit', url);
+//   }
+// });
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
   return (
@@ -25,11 +25,11 @@ function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
         <meta property='og:locale' content='ru_RU' />
       </Head>
       <Component {...pageProps} />
-      <YMInitializer
+      {/* <YMInitializer
         accounts={[]}
         options={{ webvisor: true, defer: true }}
         version='2'
-      />
+      /> */}
     </>
   );
 }
